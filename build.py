dda625263acd3176a6353e9e15fbce1b1e5083d7
theshaneyu <shaneyu07@gmail.com>
2017-04-08 16:22:05 +0000

    Changed the whole project to python3 version (mongoDB uri suspended), build class completed

---
+++ build.py
@@ -0,0 +1,112 @@
+# author: Shane Yu  date: April 8, 2017
+import subprocess
+import logging
+
+
+class build(object):
+    """
+    build class can build the word2vec model automatically from downloading the wiki raw data all the way to the training porcess,
+    and the model will be created in the CURRENT directory.
+
+    constructors to be initialized:
+    1) path of customized jieba dictionarty
+    2) path of stopwords
+    3) dimension of the model to be trained(integer)
+
+    ps. An extra directiory will be created during the process.
+    """
+    def __init__(self, jiebaDictPath, stopwordsPath, dimension):
+        self.jiebaDictPath = jiebaDictPath
+        self.stopwordsPath = stopwordsPath
+        self.dimension = dimension
+
+
+    def creatBuildDir(self):
+        subprocess.call(['mkdir', 'build'])
+
+
+    def getWiki(self):
+        subprocess.call(['wget', 'https://dumps.wikimedia.org/zhwiki/latest/zhwiki-latest-pages-articles.xml.bz2', '-P', './build/'])
+
+
+    def wikiToTxt(self):
+        # This function takes about 25 minutes
+        from gensim.corpora import WikiCorpus
+
+        logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+        wiki_corpus = WikiCorpus('./build/zhwiki-latest-pages-articles.xml.bz2', dictionary={})
+        
+        texts_num = 0
+        with open('./build/wiki_texts.txt', 'w', encoding='utf-8') as output:
+            for text in wiki_corpus.get_texts():
+                output.write(b' '.join(text).decode('utf-8') + '\n')
+                texts_num += 1
+                if texts_num % 10000 == 0:
+                    logging.info("已處理 %d 篇文章" % texts_num)
+
+
+    def opencc(self):
+        subprocess.call(['opencc', '-i', './build/wiki_texts.txt', '-o', './build/wiki_zh_tw.txt'])
+
+
+    def segmentation(self):
+        # takes about 30 minutes
+        import jieba
+
+        logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+        # jieba custom setting.
+        jieba.set_dictionary(self.jiebaDictPath)
+
+        # load stopwords set
+        stopwordset = set()
+        with open(self.stopwordsPath, 'r', encoding='utf-8') as sw:
+            for line in sw:
+                stopwordset.add(line.strip('\n'))
+
+        output = open('./build/wiki_seg.txt', 'w')
+        
+        texts_num = 0
+        
+        with open('./build/wiki_zh_tw.txt','r') as content :
+            for line in content:
+                words = jieba.cut(line, cut_all=False)
+                for word in words:
+                    if word not in stopwordset:
+                        output.write(word +' ')
+                texts_num += 1
+                if texts_num % 10000 == 0:
+                    logging.info("已完成前 %d 行的斷詞" % texts_num)
+        output.close()
+
+
+    def train(self):
+        from gensim.models import word2vec
+        import multiprocessing
+
+        logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+        sentences = word2vec.Text8Corpus('./build/wiki_seg.txt')
+        model = word2vec.Word2Vec(sentences, size=self.dimension, workers=multiprocessing.cpu_count())
+
+        # Save our model.
+        model.wv.save_word2vec_format('./med' + str(self.dimension) + '.model.bin', binary=True)
+
+
+    def exec(self):
+        print('========================== 開始下載wiki壓縮檔 ==========================')
+        self.getWiki()
+        print('========================== wiki壓縮檔下載完畢，開始將壓縮檔轉成文字檔 ==========================')
+        self.wikiToTxt()
+        print('========================== 壓縮檔轉文字檔過程完畢，開始繁轉簡過程 ==========================')
+        self.opencc()
+        print('========================== 繁轉簡過程完畢，開始斷詞 ==========================')
+        self.segmentation()
+        print('========================== 斷詞完畢，開始訓練model ==========================')
+        self.train()
+        print('========================== ' + str(self.dimension) + '維model訓練完畢，model存放在當前目錄 ==========================')
+
+
+
+if __name__ == '__main__':
+    obj = build('jieba_dict/dict.txt.big.txt', 'jieba_dict/stopwords.txt', 400)
+    obj.exec()
---
+++ kem.py
@@ -0,0 +1,68 @@
+# author: Shane Yu  date: April 8, 2017
+import json
+
+class KemMongoCache(object):
+    """
+    KemMongoCache class uses MongoDB as a cache to accelerate the process of querying kem model,
+    getTerms() function returns a list of query result from MongoDB if the query term exists in
+    the database(fast), and only do the gensim built-in query function when the query term is not
+    in the database(slow).
+    """
+    def __init__(self, uri, model_path = './KEM/med250.model.bin'):
+        from pymongo import MongoClient
+        self.client = MongoClient(uri)
+        self.db = self.client['kem']
+        self.coll = self.db['kem_coll']
+        self.model_path = model_path
+        
+
+    def getTerms(self, query, num):
+        """
+        input: query term, number of top n
+        output: query result in json formmat
+        """
+        result = self.coll.find({'Term':query}, {'Result':1, '_id':False}).limit(1) # print(type(result)) # <class 'pymongo.cursor.Cursor'>
+        if result.count() == 0:
+            resultJson = self.getJsonResult(query, num)
+            self.insertMongo(query, resultJson)
+            return resultJson
+
+        return (list(result)[0])['Result']
+
+
+    def insertMongo(self, queryTerm, resultList):
+        state = self.coll.insert({'Term':queryTerm, 'Result':resultList})
+        print(state)
+
+
+    def getJsonResult(self, queryStr, number):
+        """
+        input: query term
+        output: query result from gensim built-in query function in json formmat
+        """
+        from gensim import models
+        from gensim.models import word2vec
+        try:
+            print('Running most_similar function')
+            model = models.KeyedVectors.load_word2vec_format(self.model_path, binary=True)
+            res = model.most_similar(queryStr, topn = number) # most_similar return a list
+            print(queryStr + '相似詞前' + str(number) + '排序')
+            resJson = json.dumps(res)
+
+            return resJson
+
+        except Exception as e:
+            print(repr(e))
+
+
+
+if __name__ == '__main__':
+    """
+    due to the base directory settings of django, the model_path needs to be different when
+    testing with this section.
+    """
+    import sys
+    obj = KemMongoCache('mongodb://140.120.13.244:7777/', model_path = './med250.model.bin')
+    temp = obj.getTerms(sys.argv[1], 100)
+    for item in json.loads(temp):
+        print(item)
