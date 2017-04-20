1d3de29f13d43a888db413780ed98121ec3653dd
david <davidtnfsh@gmail.com>
2017-04-20 17:48:39 +0800

    declare specific version number

--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ setup(
     packages=['kem'],
     package_dir={'kem':'kem'},
     package_data={'kem':['management/commands/*']},
-    version = '1.5',
+    version = '1.6',
     description = 'A django App for kem',
     author = ['davidtnfsh', 'theshaneyu'],
     author_email = 'davidtnfsh@gmail.com',
     url = 'https://github.com/udicatnchu/kem',
-    download_url = 'https://github.com/udicatnchu/kem/archive/v1.5.tar.gz',
+    download_url = 'https://github.com/udicatnchu/kem/archive/v1.6.tar.gz',
     keywords = ['kem', 'gensim'],
     classifiers = [],
     license='GPL3.0',
     install_requires=[
-        'numpy==1.11.1',
+        'numpy==1.12.1',
         'scipy==0.18.0',
-        'gensim==0.13.2'
+        'gensim==2.0.0'
     ],
     zip_safe=True,
 )
